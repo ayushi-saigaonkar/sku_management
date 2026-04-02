807e4ee732cf24fee8e40b161de3c1f22a5b9be3
ayushi.saigaonkar <ayushi.saigaonkar@mirraw.com>
2026-04-02 18:47:12 +0530

    fixes for error messages and lineitems creation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,15 +8,29 @@ class OrdersController < ApplicationController
     # if no - mark previous order's lineitems original false
     existing_order = Order.find(external_id: params[:external_id])
 
-    if existing_order.present? & existing_order.locked_at.present? && existing_order.created_at > (Time.now - 15.minutes)
+    if existing_order.present? & existing_order.locked_at.present? && existing_order.created_at < 15.minutes.ago
       render json: { error: "Unprocessed Entity" }, status: 422
     else
       existing_order.line_items.update_all(original: false)
-      order.line_items.update_all(original: true)
+      params[:line_items].each do |item|
+          existing_order.line_items.create!(
+            sku: item[:sku],
+            quantity: item[:quantity],
+            original: true
+          )
+        end
+        existing_order.update!(placed_at: params[:placed_at])
     end
 
     # save new original
     if order.save
+      params[:line_items].each do |item|
+        order.line_items.create!(
+          sku: item[:sku],
+          quantity: item[:quantity],
+          original: true
+        )
+      end
       CalculateSkuStatsJob.perform_later(order.id)
       render json: order, status: :created
     else
@@ -27,9 +41,12 @@ class OrdersController < ApplicationController
 
   def lock
     order = Order.find(id: params[:id])
-    order.locked_at = Time.now() # can use Time.current()
+    order.update!(locked_at: Time.now) # can use Time.current()
     CalculateSkuStatsJob.perform_later(order.id)
+    render json: order, status: :ok
   end
+
+
 
   private
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,4 @@
 class Order < ApplicationRecord
-  validates :external_id, :placed_at, presence: true
-  has_many :line_items
+  validates :external_id, :placed_at, presence: true, uniqueness: true
+  has_many :line_items, dependent: :destroy
 end
